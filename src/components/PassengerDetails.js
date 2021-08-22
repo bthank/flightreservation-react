78f2a4045cd515a9ff87bd4d8245894fe63bad46
bthank <bthankachan@yahoo.com>
2021-08-21 21:50:55 -0400

    Capture passenger details and display them on UI

--- src/components/PassengerDetails.js
+++ src/components/PassengerDetails.js
@@ -6,7 +6,7 @@ class PassengerDetails extends React.Component {
     state = {};
 
     componentWillMount(){
-        axios.get("http://localhost:8080/flightServices/flights/" + this.props.match.params.flightId)
+        axios.get("/flightServices/flights/" + this.props.match.params.flightId)
             .then(response => {
                 /* the flight info that comes in will become the new state  */
                 this.setState(response.data);
@@ -22,6 +22,21 @@ class PassengerDetails extends React.Component {
                 Departure City   : {this.state.departureCity} <br/>
                 Arrival City     : {this.state.arrivalCity} <br/>
                 Date of Departure: {this.state.dateOfDeparture} <br/>
+
+                <h2>Passenger Details</h2>
+                <form>
+                    First Name: <input type="text" name="passengerFirstName" onChange={(event) => {this.passengerFirstName = event.target.value}}/> <br/>
+                    Middle Name: <input type="text" name="passengerMiddleName" onChange={(event) => {this.passengerMiddleName = event.target.value}}/> <br/>
+                    Last Name: <input type="text" name="passengerLastName" onChange={(event) => {this.passengerLastName = event.target.value}}/> <br/>
+                    Email: <input type="text" name="passengerEmail" onChange={(event) => {this.passengerEmail = event.target.value}}/> <br/>
+                    Phone Number: <input type="text" name="passengerPhone" onChange={(event) => {this.passengerPhone = event.target.value}}/> <br/>
+                    <h2>Credit Card Details</h2>
+                    Credit Card No: <input type="text" name="cCardNumber" onChange={(event) => {this.cCardNumber = event.target.value}}/> <br/>
+                    Expiration Date: <input type="text" name="expirationDate" onChange={(event) => {this.expirationDate = event.target.value}}/> <br/>
+                    Security Code: <input type="text" name="securityCode" onChange={(event) => {this.securityCode = event.target.value}}/> <br/>
+                    <button>Confirm</button>
+                </form>
+
             </div>
                 );
 
